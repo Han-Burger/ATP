e431d667ce5ade3a4dcd024c2fb07745c67cc849
Han Hong <hh583@cornell.edu>
2017-10-30 21:37:54 -0400

    update player

--- atp/player/player.py
+++ atp/player/player.py
@@ -1,2 +1,24 @@
+from atp.webscraper import player_rank_history_parser
 class Player(object):
-    pass
+
+    def __init__(self, rank_history):
+        """
+        :param rank_history: a rank history dataframe
+        """
+        self.rank_history = rank_history
+
+    @classmethod
+    def from_webpage(cls, url):
+        df = player_rank_history_parser(url)
+        return cls(df)
+
+    @classmethod
+    def from_local(cls, path):
+        # TODO: read from pickle or csv
+        pass
+
+
+if __name__ == '__main__':
+    url = r'http://www.atpworldtour.com/en/players/rafael-nadal/n409/rankings-history'
+    rn = Player.from_webpage(url)
+    print(rn.rank_history)
--- atp/webscraper/webscraper.py
+++ atp/webscraper/webscraper.py
@@ -8,19 +8,18 @@ rank_history_header = load_header('rank_history_df.json')
 
 def player_rank_history_parser(url):
     """
-    :param url:
+    :param url: atp player raking history page url
     :return:
     """
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     rs = soup.find('div', attrs={'id': 'playerRankHistoryContainer'})
     tb = rs.find('tbody').find_all('tr')
-    return [[parse_table(d.text.strip()) for d in row.find_all('td')] for row in tb]
-
+    mat = [[parse_table(d.text.strip()) for d in row.find_all('td')] for row in tb]
+    return pd.DataFrame.from_records(mat, columns = rank_history_header)
 
 
 
 if __name__ == '__main__':
     url = r'http://www.atpworldtour.com/en/players/rafael-nadal/n409/rankings-history'
-    mat = player_rank_history_parser(url)
-    print(pd.DataFrame.from_records(mat, columns = rank_history_header))
+    print (player_rank_history_parser(url))
